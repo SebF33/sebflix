999593734c6e0610f046d25038a990dde911b21e
Sébastien FLOURIOT <sebastien.flouriot@gmail.com>
2021-07-14 20:30:51 +0200

    various fixes

--- src/templates/background.php
+++ src/templates/background.php
@@ -4,7 +4,7 @@
                             echo '"/src/thumbnails/' . $fanart['cachedurl'] . '"';
                           } elseif ($type == 'filmography' and $id == '26276') {
                             echo '"/img/bg_chaplin.png"';
-                          } elseif ($type == 'actors' or $type == 'beststudios' or $type == 'direction' or $type == 'genres' or $type == 'movies' or $type == 'sets' or $type == 'studios' or $type == 'tvshows') {
+                          } elseif ($type == 'achievement' or $type == 'actors' or $type == 'beststudios' or $type == 'direction' or $type == 'genres' or $type == 'movies' or $type == 'sets' or $type == 'studios' or $type == 'tvshows') {
                             echo '"/img/bg_' . $type . '.png"';
                           } elseif ($category == 'movies' or $category == 'spectacles' or $category == 'tvshows') {
                             echo '"/img/bg_' . $category . '.png"';
